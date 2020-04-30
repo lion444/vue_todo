6e7470f801c521e975623c1ca498a82f968f20ba
Leon, Mike <none>
2020-04-30 12:56:43 -0500

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{count}}</h2>
+    <p>
+      <button v-on:click="add()">add</button>
+    </p>
+    <p>
+      <button v-on:click="changeColor()">change color</button>
+    </p>
+    <p>
+      <input type="text" v-model="textColor" />
+    </p>
+    <p>
+      <button v-on:click="getWeather()">check weather</button>
+    </p>
+    <p id="weather">
+      Car color is {{car.color}}
+      <br />
+      Weather: {{weather}}
+      <br />
+
+      <br />
+    </p>
+    And has {{car.wheelCount}} wheels
+  </div>
+</template>
+
+<script>
+let Car = function(color, wheelCount) {
+  this.color = color;
+  this.wheelCount = wheelCount;
+};
+let blueCar = new Car("blue", 4);
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      car: blueCar,
+      car2: new Car("Red", 4),
+      textColor: "",
+      weather: ""
+    };
+  },
+  methods: {
+    add: function() {
+      blueCar.wheelCount++;
+    },
+    changeColor: function() {
+      this.car.color = this.textColor;
+    },
+    getWeather: function() {
+      this.weather = getWeatherFromAPI("hot");
+    }
+  }
+};
+
+let getWeatherFromAPI = w => {
+  return w;
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#weather {
+  color: aqua;
+  background-color: black;
+
+  margin: 40px 0 0;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <div id="divider">WEATHER</div>
+    <p>
+      <input type="text" v-model="city" />
+    </p>
+    <p>
+      <button v-on:click="changeCity()">changeCity</button>
+    </p>
+    <p id="weather">{{weather.city}} is {{weather.temp}} and it is {{weather.raining}}</p>
+  </div>
+</template>
+
+<script>
+let Weather = function(city, temp, raining) {
+  this.city = city;
+  this.temp = temp;
+  this.raining = raining;
+};
+
+export default {
+  name: "Weather",
+  props: {},
+  data: function() {
+    return {
+      weather: new Weather("60050", 55, false),
+      city: ""
+    };
+  },
+  methods: {
+    changeCity: function() {
+      this.weather.city = this.city;
+    },
+
+    getWeather: function() {
+      this.weather = getWeatherFromAPI("hot");
+    }
+  }
+};
+
+let getWeatherFromAPI = w => {
+  return w;
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#weather {
+  color: aqua;
+  background-color: black;
+
+  margin: 40px 0 0;
+}
+#divider {
+  background-color: blue;
+  color: white;
+}
+</style>
